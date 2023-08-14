fa3c23af0bc10226952b12f19ca20dc1d5c9b521
Jasjot Singh <Jasjotsingh103@gmail.com>
2023-08-14 19:03:52 +0530

    Initial commit

---
+++ src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,7 @@
+package org.example.config;
+
+public class AppConfig {
+    public static final String DOWNLOAD_PATH="C:\\Users\\harjot singh\\IdeaProjects\\JavaIDM";
+    //other config information
+
+}
